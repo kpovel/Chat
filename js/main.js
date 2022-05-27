f7f4fead1afdca814ec7b23fed1ea0635d858752
kpovel <kar.pavel123@gmail.com>
2022-05-27 22:56:25 +0300

    add 2 popup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import {UI_ELEMENTS} from './view.js';
-import format from 'date-fns/format';
+import {UI_ELEMENTS, sendMessage} from './view.js';
 
 UI_ELEMENTS.MESSAGE.INPUT_FIELD.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
@@ -8,14 +7,3 @@ UI_ELEMENTS.MESSAGE.INPUT_FIELD.addEventListener('keypress', function (e) {
 });
 UI_ELEMENTS.MESSAGE.BUTTON.addEventListener('click', sendMessage);
 
-
-function sendMessage() {
-    const message = UI_ELEMENTS.MESSAGE.INPUT_FIELD.value;
-    const templateElement = UI_ELEMENTS.TEMPLATE.MESSAGE.content.cloneNode(true);
-    if (message) {
-        templateElement.firstElementChild.firstElementChild.textContent = `Я: ${message}`;
-        templateElement.firstElementChild.lastElementChild.textContent = format(new Date(), 'HH:mm');
-        UI_ELEMENTS.CHAT.append(templateElement);
-        UI_ELEMENTS.MESSAGE.INPUT_FIELD.value = null;
-    }
-}
--- js/view.js
+++ js/view.js
@@ -1,3 +1,5 @@
+import format from 'date-fns/format';
+
 export const UI_ELEMENTS = {
     SETTING: {
         BUTTON: document.getElementById('buttonSetting'),
@@ -31,3 +33,13 @@ function switchOnMainTab() {
     TABS.MAIN.style.display = 'flex';
 }
 
+export function sendMessage() {
+    const message = UI_ELEMENTS.MESSAGE.INPUT_FIELD.value;
+    const templateElement = UI_ELEMENTS.TEMPLATE.MESSAGE.content.cloneNode(true);
+    if (message) {
+        templateElement.firstElementChild.firstElementChild.textContent = `Я: ${message}`;
+        templateElement.firstElementChild.lastElementChild.textContent = format(new Date(), 'HH:mm');
+        UI_ELEMENTS.CHAT.append(templateElement);
+        UI_ELEMENTS.MESSAGE.INPUT_FIELD.value = null;
+    }
+}
